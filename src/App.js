9e7f15e72cdef4dc69cb08709f5cf4eb45133751
Luiza Schmidt Mescolotto <lsmescolotto@gmail.com>
2021-12-07 19:31:37 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import GlobalStyle from "./styles/global";
+import Routes from "./routes";
+
+const App = () => {
+  return (
+    <main>
+      <GlobalStyle />
+      <Routes />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/routes/index.jsx
@@ -0,0 +1,28 @@
+import { Route, Switch } from "react-router-dom";
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <h1>Home</h1>
+      </Route>
+      <Route path="/login">
+        <h1>Login</h1>
+      </Route>
+      <Route path="/signup">
+        <h1>Signup</h1>
+      </Route>
+      <Route path="/dashboard">
+        <h1>Dashboard</h1>
+      </Route>
+      <Route path="/groups_list">
+        <h1>Groups List</h1>
+      </Route>
+      <Route path="/group/:group_id">
+        <h1>Group</h1>
+      </Route>
+    </Switch>
+  );
+};
+
+export default Routes;
