4e5658dd121d7feddfdff6950685ed7b7a572721
Fernao Shiotsuki <fernaospalma@gmail.com>
2021-12-08 20:50:22 -0300

    fix: add service api

--- src/providers/user/user.jsx
+++ src/providers/user/user.jsx
@@ -1,19 +1,19 @@
-import { createContext, useContext, useState } from "react";
-
+import { createContext, useState } from "react";
+import api from "../../services/api";
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [token, setToken] = useState("");
   const userRegister = (payload) => {
-
+    api
       .post("https://kenzie-habits.herokuapp.com/users/", payload)
       .then((res) => console.log(res))
       .catch((err) => console.log(err.message));
   };
 
   const userLogin = (payload) => {
-
-      api.post("https://kenzie-habits.herokuapp.com/sessions/", payload)
+    api
+      .post("https://kenzie-habits.herokuapp.com/sessions/", payload)
       .then((res) => {
         setToken(JSON.stringify(res.data.access));
       })
