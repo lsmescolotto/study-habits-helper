b216a17dcb6e4001342708fc5184a60f3a7f8e9e
Fernao Shiotsuki <fernaospalma@gmail.com>
2021-12-08 18:11:56 -0300

    fix: add api service

--- src/providers/user/user.js
+++ src/providers/user/user.js
@@ -1,19 +1,21 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import api from "../../services/api";
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [token, setToken] = useState("");
+  const [authenticated, setAuthenticated] = useState(false);
   const userRegister = (payload) => {
-    axios
-      .post("https://kenzie-habits.herokuapp.com/users/", payload)
+    api
+      .post("users/", payload)
       .then((res) => console.log(res))
       .catch((err) => console.log(err.message));
   };
 
   const userLogin = (payload) => {
-    axios
-      .post("https://kenzie-habits.herokuapp.com/sessions/", payload)
+    api
+      .post("sessions/", payload)
       .then((res) => {
         setToken(JSON.stringify(res.data.access));
       })
@@ -21,9 +23,14 @@ export const UserProvider = ({ children }) => {
         console.log(err.message);
       });
   };
+  useEffect(() => {
+    setAuthenticated(true);
+  }, [token]);
 
   return (
-    <UserContext.Provider value={{ userLogin, userRegister, token }}>
+    <UserContext.Provider
+      value={{ userLogin, userRegister, token, authenticated }}
+    >
       {children}
     </UserContext.Provider>
   );
