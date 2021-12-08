42cd819b366591ef77ab83ff6876e653251f6562
Fernao Shiotsuki <fernaospalma@gmail.com>
2021-12-08 20:44:17 -0300

    saving changes

--- src/providers/user/user.jsx
+++ src/providers/user/user.jsx
@@ -5,15 +5,15 @@ export const UserContext = createContext();
 export const UserProvider = ({ children }) => {
   const [token, setToken] = useState("");
   const userRegister = (payload) => {
-    axios
+
       .post("https://kenzie-habits.herokuapp.com/users/", payload)
       .then((res) => console.log(res))
       .catch((err) => console.log(err.message));
   };
 
   const userLogin = (payload) => {
-    axios
-      .post("https://kenzie-habits.herokuapp.com/sessions/", payload)
+
+      api.post("https://kenzie-habits.herokuapp.com/sessions/", payload)
       .then((res) => {
         setToken(JSON.stringify(res.data.access));
       })
@@ -28,5 +28,3 @@ export const UserProvider = ({ children }) => {
     </UserContext.Provider>
   );
 };
-
-export const useUser = useContext(UserContext);
