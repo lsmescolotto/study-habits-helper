64a784aa548e0d64a0d88148b442808192ccb3c4
Luan <luandiniz91@hotmail.com>
2021-12-10 18:43:22 -0300

    feature/container-user-component

---
+++ src/components/containerUser/index.jsx
@@ -0,0 +1,16 @@
+import Button from "../button";
+
+const ContainerUser = () => {
+  return (
+    <div>
+      <div>
+        <h3>Perfil</h3>
+      </div>
+      <div>Username</div>
+      <div>Email</div>
+      <Button>Editar Perfil</Button>
+    </div>
+  );
+};
+
+export default ContainerUser;
