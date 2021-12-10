3b669e05ac012abfe8338628038db340e843b7d6
Cristian Tacca <christacca@gmail.com>
2021-12-10 17:30:58 -0300

    fix: fixed the structure for groups

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,17 +1,18 @@
 import { Container } from "./styles";
 import Button from "../button";
 
-const Header = ({ dashboard = false, id }) => {
+const Header = ({ dashboard = false, id, group = false }) => {
   return (
     <Container>
       <figure>
         <img src="" alt="logo" />
       </figure>
-      {dashboard === true ? (
+      {!dashboard && (
         <div>
           <div>icons</div>
         </div>
-      ) : (
+      )}
+      {!group && (
         <div>
           <Button>Editar</Button>
           {!!id && <Button>Sair do grupo</Button>}
