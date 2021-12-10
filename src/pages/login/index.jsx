3ae6f44b655474b7ea00b61950aa28d4a049c9d8
Luan <luandiniz91@hotmail.com>
2021-12-10 13:42:41 -0300

    fix: change login from email to username

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -14,7 +14,7 @@ const Login = () => {
   const history = useHistory();
 
   const schema = yup.object().shape({
-    email: yup.string().email("Email inválido").required("Campo Obrigatório"),
+    username: yup.string().required("Campo Obrigatório"),
     password: yup.string().required("Campo Obrigatório"),
   });
 
@@ -36,11 +36,11 @@ const Login = () => {
       <h2>Login</h2>
       <form onSubmit={handleSubmit(onSubmitFunction)}>
         <Input
-          label="Digite seu Email"
-          placeholder="E-mail"
+          label="Digite seu Username"
+          placeholder="Username"
           register={register}
-          name="email"
-          error={errors.email?.message}
+          name="username"
+          error={errors.username?.message}
         />
 
         <Input
