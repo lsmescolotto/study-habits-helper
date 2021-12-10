e297d7c2e23b6202b842fa33a79f56c76e51ae5e
lsmescolotto <88347963+lsmescolotto@users.noreply.github.com>
2021-12-10 13:20:04 -0300

    Revert "Feature/dashboard page structure"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import GlobalStyle from "./styles/global";
 import Routes from "./routes";
-import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <main>
       <GlobalStyle />
       <Routes />
-      <Toaster />
     </main>
   );
 };
--- src/components/button/index.jsx
+++
@@ -1,9 +0,0 @@
-const Button = ({ onClick, children, ...rest }) => {
-  return (
-    <button onClick={onClick} {...rest}>
-      {children}
-    </button>
-  );
-};
-
-export default Button;
