155606f9b2a48740adee1e1ca2af5fccbb88813d
Cristian Tacca <christacca@gmail.com>
2021-12-11 23:02:42 -0300

    feat: done the logic for habits and groups cards

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useContext } from "react";
 import { Container, HabitsContainer, GroupsContainer } from "./styles.js";
 import Header from "../../components/header";
 import Footer from "../../components/footer";
+import ContainerDashboard from "../../components/containerDashboard";
+import DisplayCard from "../../components/displayCard/index.jsx";
+import { HabitsContext } from "../../providers/habits/habits.js";
+import { GroupContext } from "../../providers/groups/groups.js";
 
 const Dashboard = () => {
+  const { habitsList, getHabitsAxios } = useContext(HabitsContext);
+  const { subscriptions, getGroupsSubscriptions } = useContext(GroupContext);
+
+  useEffect(() => {
+    getHabitsAxios();
+    getGroupsSubscriptions();
+  }, []);
+
   return (
     <Container>
       <Header dashboard />
       <div className="cards-containers">
-        <HabitsContainer>HABITOS</HabitsContainer>
-        <GroupsContainer>GROUPS</GroupsContainer>
+        <HabitsContainer>
+          <ContainerDashboard text="Habitos" />
+          {habitsList[0] && <DisplayCard data={habitsList} />}
+        </HabitsContainer>
+        <GroupsContainer>
+          <ContainerDashboard text="Grupos" />
+          {subscriptions[0] && (
+            <DisplayCard type="group" data={subscriptions} />
+          )}
+        </GroupsContainer>
       </div>
       <Footer />
     </Container>
