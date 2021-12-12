e059880dfeed486418b7edfadf44046c3475d922
Sarah Scardini <sarahscardini@gmail.com>
2021-12-12 14:53:28 -0300

    feat: add new div(popUp container), now container disable background while popUp is open

--- src/components/popUps/popUpBase/index.jsx
+++ src/components/popUps/popUpBase/index.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import Button from "../../button";
-import { Container, PopUpHeader, PopUpBody } from "./styles.js";
+import { Container, PopUpContainer, PopUpHeader, PopUpBody } from "./styles.js";
 
 const PopUpBase = ({ title, children, closePopUp }) => {
   return (
     <Container>
-      <PopUpHeader>
-        <h3>{title}</h3>
-        <Button
-          onClick={closePopUp}
-          children="X"
-          className="closePopUP-button"
-        />
-      </PopUpHeader>
-      <PopUpBody>{children}</PopUpBody>
+      <PopUpContainer>
+        <PopUpHeader>
+          <h3>{title}</h3>
+          <Button
+            onClick={closePopUp}
+            children="X"
+            className="closePopUP-button"
+          />
+        </PopUpHeader>
+        <PopUpBody>{children}</PopUpBody>
+      </PopUpContainer>
     </Container>
   );
 };
