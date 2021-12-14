33f9b0fc722e5b0bc8f5d006c3621a54b0a3194d
Sarah Scardini <sarahscardini@gmail.com>
2021-12-14 09:56:52 -0300

    style: add image and icon and change color, font and sizes of team page

--- src/pages/team/styles.js
+++ src/pages/team/styles.js
@@ -1,53 +1,114 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  margin-top: 20px;
-
-  width: 100vw;
-  height: 90vh;
-
+export const TeamContainer = styled.div`
   display: flex;
-  justify-content: flex-start;
-  align-items: center;
+  flex-flow: column;
+  height: 100vh;
+  justify-content: space-between;
+`;
 
-  figure img {
-    width: 50%;
-    height: 100%;
+export const Container = styled.div`
+  align-items: center;
+  display: flex;
+  height: 90vh;
+  justify-content: flex-start;
+  justify-content: space-around;
+  margin-top: 20px;
+  width: 100vw;
+
+  .team_gif {
+    display: none;
+  }
+
+  @media (min-width: 1024px) {
+    .team_gif {
+      display: flex;
+      max-width: 615px;
+      width: 45vw;
+    }
   }
 `;
 
 export const InfoTeam = styled.div`
+  max-width: 470px;
   width: 90%;
-  height: 100%;
 `;
 
 export const Content = styled.div`
-  margin: 15px;
-
-  height: 10%;
-
+  align-items: center;
   display: flex;
   justify-content: center;
-  align-items: center;
+  margin: 15px;
 
   figure {
-    width: 100px;
     height: 50px;
+    width: 100px;
   }
 
   figure img {
-    width: 50px;
-    height: 50px;
-
     border-radius: 50%;
+    height: 50px;
+    width: 50px;
   }
 `;
 
 export const About = styled.div`
-  width: fit-content;
-
+  align-items: flex-start;
+  color: var(--black-50);
   display: flex;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
-  align-items: center;
+  flex-direction: column;
+  font-family: var(--main-font);
+  font-size: 14px;
+  justify-content: center;
+  line-height: 1.25rem;
+  width: 200px;
+
+  a {
+    cursor: pointer;
+    height: 20px;
+    width: 20px;
+  }
+
+  svg {
+    color: #0a66c2;
+    font-size: 20px;
+  }
+
+  svg:hover {
+    color: var(--blue-50);
+  }
+
+  svg:active {
+    color: var(--blue-60);
+  }
+
+  h4 {
+    margin-right: 10px;
+  }
+
+  strong {
+    font-weight: 500;
+  }
+
+  @media (min-width: 500px) {
+    align-items: center;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 320px;
+  }
+
+  @media (min-width: 1024px) {
+    font-size: 16px;
+    margin: 20px 15px;
+    width: 350px;
+
+    a {
+      height: 25px;
+      width: 25px;
+    }
+
+    svg {
+      font-size: 25px;
+    }
+  }
 `;
