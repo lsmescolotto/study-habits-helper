07ce5bda3a6f810ae96160d01ea11567581574fd
Airton Senna <airtonsenna13@gmail.com>
2021-12-14 19:28:34 -0300

    style: style-components added

--- src/pages/signup/styles.js
+++ src/pages/signup/styles.js
@@ -1,59 +1,42 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-  
-  .desktopStyle {
-    display: flex;
-    justify-content: space-around;
-  }
-
-  .backgroundImg {
-    display: none;
-  }
+export const Image = styled.img`
+  display: none;
 
   @media only screen and (min-width: 900px){
-  .backgroundImg {
-  display: inline-block;
+    display: inline-block;
   }
-}
+
+`
+
+export const Desktop = styled.div`
+  display: flex;
+  justify-content: space-around;
+`
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 
 `;
 
+
 export const Content = styled.section`
-  height: 80vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-evenly;
-  font-style: roboto;
 
-  .backgroundImg {
-    display: none;
-  }
+height: 80vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: space-evenly;
+font-style: var(--main-font);
 
-  h2 {
+h2 {
     font-size: 28px;
-    font-family: anton;
-    color: #323232;
+    font-family: var(--title-font);
+    color: var(--black-100);
 
-  }
-
-  .signUpBox {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    height: fit-content;
-    width: 300px;
-
-
-    border-radius: 8px;
-    box-shadow: 4px 5px 4px var(--grey-90);
   }
 
   form {
@@ -61,20 +44,33 @@ export const Content = styled.section`
     flex-direction: column;
     align-items: center;
   }
-
+  
   input {
     width: 250px;
   }
-
-  .signUpBox-login {
-    width: 250px;
-    display: flex;
-    justify-content: space-around;
-    padding: 20px 30px;
-    border-radius: 8px;
-    box-shadow: 4px 5px 4px var(--grey-90);
-  }
-
-
   
+`;
+
+
+export const SignUpBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  height: fit-content;
+  width: 300px;
+
+
+  border-radius: 8px;
+  box-shadow: 4px 5px 4px var(--grey-90);
+`;
+
+export const LoginRedirectBox = styled.div`
+  width: 300px;
+  display: flex;
+  justify-content: space-around;
+  text-align: center;
+  padding: 20px 30px;
+  border-radius: 8px;
+  box-shadow: 4px 5px 4px var(--grey-90);
 `;
