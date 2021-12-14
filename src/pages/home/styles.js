52ef37c5513185685cb1fbbb0a324dbaa9685f6d
Luiza Schmidt Mescolotto <lsmescolotto@gmail.com>
2021-12-14 16:22:56 -0300

    style: home page content position and media queries

--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,33 +1,6 @@
 import styled from "styled-components";
 import homeImage from "../../assets/img/homeImg.gif";
 
-export const RedirectContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  height: 50vh;
-  width: 60vw;
-  text-align: center;
-
-  h1 {
-    font-size: 29px;
-    font-weight: bold;
-  }
-
-  h2,
-  h3 {
-    font-size: 24px;
-  }
-
-  a {
-    margin-left: 5px;
-  }
-
-  @media screen and (min-width: 1024px) {
-    width: 40vw;
-  }
-`;
-
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -37,10 +10,82 @@ export const Container = styled.div`
   font-family: var(--main-font);
 `;
 
-export const ImageContainer = styled.div`
+export const Content = styled.main`
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media (min-width: 1024px) {
+    justify-content: space-around;
+    align-items: center;
+  }
+`;
+
+export const RedirectContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  height: 50vh;
+  width: 60vw;
+  text-align: center;
+
+  header {
+    h1 {
+      font-size: 29px;
+      font-weight: bold;
+    }
+
+    h2 {
+      font-size: 26px;
+    }
+  }
+
+  h3 {
+    font-size: 24px;
+  }
+
+  h4 {
+    font-size: 14px;
+  }
+
+  a {
+    margin-left: 5px;
+  }
+
   @media screen and (min-width: 1024px) {
-    width: 40vw;
-    height: 70vh;
+    width: 45vw;
+
+    header {
+      display: flex;
+      justify-content: center;
+      align-items: flex-start;
+
+      h1 {
+        margin-right: 7px;
+        font-size: 32px;
+      }
+
+      h2 {
+        font-size: 29px;
+      }
+    }
+
+    h3 {
+      font-size: 26px;
+    }
+
+    h4 {
+      font-size: 18px;
+    }
+  }
+`;
+
+export const ImageContainer = styled.div`
+  @media (min-width: 1024px) {
+    width: 495px;
+    height: 579px;
     background: url(${homeImage}) no-repeat center;
+    background-size: 479px 372px;
   }
 `;
