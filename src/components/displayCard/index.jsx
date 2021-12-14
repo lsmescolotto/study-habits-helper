eb7c7a14a462e055540eaf46c33900a6376ae147
Fernao Shiotsuki <fernaospalma@gmail.com>
2021-12-14 18:13:40 -0300

    feat: add details inside the card body

--- src/components/displayCard/index.jsx
+++ src/components/displayCard/index.jsx
@@ -7,15 +7,17 @@ const DisplayCard = ({ type = "", data }) => {
               <h3>{current.name}</h3>
               <h4>{current.category}</h4>
               <p>{current.description}</p>
-              <p>{current.creator.username}</p>
+              <p>By: {current.creator.username}</p>
             </div>
           ))
         : data.map((current) => (
             <div>
               <h3>{current.title}</h3>
               <h4>{current.category}</h4>
-              <p>{current.achieved}</p>
-              <p>{current.how_much_achieved}</p>
+              <p>{current.difficulty}</p>
+              <p>{current.frequency}</p>
+              <p>Status: {current.achieved ? "Concluido" : "Em progresso"}</p>
+              <p>Progresso: {current.how_much_achieved}/100</p>
             </div>
           ))}
     </>
