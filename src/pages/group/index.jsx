4107cf3f9c6ba64a379f9dfa13f1819cb52fc612
lsmescolotto <88347963+lsmescolotto@users.noreply.github.com>
2021-12-15 13:57:26 -0300

    refactor: add eslint disable

--- src/pages/group/index.jsx
+++ src/pages/group/index.jsx
@@ -22,6 +22,7 @@ const Group = () => {
   useEffect(() => {
     renderGoals(groupId);
     renderActivities(groupId);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
