cdd00a0778c9b0cd47e4149e168d764b66f075f0
Luan <luandiniz91@hotmail.com>
2021-12-15 13:02:33 -0300

    style: header component style and add logout button

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,8 +1,20 @@
 import { Container } from "./styles";
 import Button from "../button";
 import Logo from "../../assets/img/logo.png";
+import { FiUser, FiLogOut, FiArrowLeftCircle } from "react-icons/fi";
+import { Link, useHistory } from "react-router-dom";
 
 const Header = ({ dashboard = false, id, group = false }) => {
+  const history = useHistory();
+  const logout = () => {
+    localStorage.clear();
+    history.push("/");
+  };
+
+  const GoToDashboard = () => {
+    history.push("/dashboard");
+  };
+
   return (
     <Container>
       <figure>
@@ -10,17 +22,24 @@ const Header = ({ dashboard = false, id, group = false }) => {
       </figure>
       {!!dashboard && (
         <div>
-          <div>icons</div>
+          <span>
+            <FiUser />
+          </span>
+          <span onClick={logout}>
+            <FiLogOut />
+          </span>
         </div>
       )}
       {!!group && (
-        <div>
+        <section>
           <Button name="button--blue button__header">Editar</Button>
           {!!id && (
             <Button name="button--red button__header">Sair do grupo</Button>
           )}
-          <div>icon</div>
-        </div>
+          <span onClick={GoToDashboard}>
+            <FiArrowLeftCircle />
+          </span>
+        </section>
       )}
     </Container>
   );
