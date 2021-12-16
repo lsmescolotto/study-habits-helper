d0d983cf0df46fcb3fc1439a5bd95bb94dd6b738
Luan <luandiniz91@hotmail.com>
2021-12-16 19:09:49 -0300

    style: popup new activities

--- src/components/popUps/newActivity/styles.js
+++ src/components/popUps/newActivity/styles.js
@@ -4,7 +4,16 @@ export const Container = styled.div`
   form {
     display: flex;
     flex-direction: column;
-    justify-content: flex-start;
+    justify-content: center;
     text-align: start;
+    padding: 0px 40px 40px 40px;
+
+    @media (max-width: 768px) {
+      padding: 0px 30px 30px 30px;
+    }
+
+    button {
+      margin-top: 20px;
+    }
   }
 `;
