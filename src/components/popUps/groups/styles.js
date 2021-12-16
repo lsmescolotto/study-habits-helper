58cf4ba80cf9652f1741c2221d6f46a872bcad90
Luan <luandiniz91@hotmail.com>
2021-12-16 19:32:33 -0300

    style: popup new group

--- src/components/popUps/groups/styles.js
+++ src/components/popUps/groups/styles.js
@@ -1,8 +1,17 @@
 import styled from "styled-components";
 
 export const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    text-align: left;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: start;
+  padding: 0px 40px 40px 40px;
 
+  @media (max-width: 768px) {
+    padding: 0px 30px 30px 30px;
+  }
+
+  button {
+    margin-top: 20px;
+  }
+`;
