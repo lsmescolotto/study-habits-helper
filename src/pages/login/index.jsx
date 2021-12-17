7b23e9752cb4b831eebb31ee8fa301cdd1d17383
Cristian Tacca <christacca@gmail.com>
2021-12-16 22:39:17 -0300

    feat: add framer motion

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -11,6 +11,7 @@ import Footer from "../../components/footer";
 import { UserContext } from "../../providers/user/user";
 
 import { Container, Content, Form, Background } from "./styles";
+import { motion } from "framer-motion";
 
 const Login = () => {
   const { userLogin } = useContext(UserContext);
@@ -33,47 +34,55 @@ const Login = () => {
   };
 
   return (
-    <Container>
-      <div>
-        <Header />
-        <Content>
-          <Background />
-          <Form>
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit(onSubmitFunction)}>
-              <Input
-                label="Digite seu Username"
-                placeholder="Username"
-                register={register}
-                name="username"
-                error={errors.username?.message}
-              />
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1 }}
+    >
+      <Container>
+        <div>
+          <Header />
+          <Content>
+            <Background />
+            <Form>
+              <h2>Login</h2>
+              <form onSubmit={handleSubmit(onSubmitFunction)}>
+                <Input
+                  label="Digite seu Username"
+                  placeholder="Username"
+                  register={register}
+                  name="username"
+                  error={errors.username?.message}
+                />
 
-              <Input
-                label="Digite sua senha"
-                type="password"
-                placeholder="Senha"
-                register={register}
-                name="password"
-                error={errors.password?.message}
-              />
+                <Input
+                  label="Digite sua senha"
+                  type="password"
+                  placeholder="Senha"
+                  register={register}
+                  name="password"
+                  error={errors.password?.message}
+                />
 
-              <Button type="submit" name="button--pink">Login</Button>
-            </form>
+                <Button type="submit" name="button--pink">
+                  Login
+                </Button>
+              </form>
 
-            <small>
-              <p>
-                Não possui conta? <Link to="/signup"> Cadastre-se</Link>
-              </p>
-            </small>
-
-          </Form>
-        </Content>
-        <span>
-          <Footer />
-        </span>
-      </div>
-    </Container>
+              <small>
+                <p>
+                  Não possui conta? <Link to="/signup"> Cadastre-se</Link>
+                </p>
+              </small>
+            </Form>
+          </Content>
+          <span>
+            <Footer />
+          </span>
+        </div>
+      </Container>
+    </motion.div>
   );
 };
 
